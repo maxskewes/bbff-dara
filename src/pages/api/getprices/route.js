4b203df598cee0045985dab0f5fef5f0dd2a34d5
Maxwell Countryman Skewes <maxskewes@gmail.com>
2024-01-04 10:24:50 -0800

    commented out old stripe call with NextApiResponse

--- src/pages/api/getprices/route.js
+++ src/pages/api/getprices/route.js
@@ -1,9 +1,9 @@
-import Stripe from "stripe";
-import { NextApiResponse } from "next/server";
+// import Stripe from "stripe";
+// import { NextApiResponse } from "next/server";
 
-export async function GET(request) {
-    const stripe = new Stripe(process.env.local.STRIPE_SECRET_KEY)
-    const prices = await stripe.prices.list();
+// export async function GET(request) {
+//     const stripe = new Stripe(process.env.local.STRIPE_SECRET_KEY)
+//     const prices = await stripe.prices.list();
 
-    return NextApiResponse.json(prices.data.reverse())
-}
+//     return NextApiResponse.json(prices.data.reverse())
+// }
