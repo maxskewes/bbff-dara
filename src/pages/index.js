38e382853b5e9dd4a2f38d2229fe4ac936436dac
Maxwell Countryman Skewes <maxskewes@gmail.com>
2024-04-26 20:28:53 -0700

    ordered bandlist by timeslot

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,9 +47,10 @@ export default function Home() {
       <Head>
         <title>Bridgetown Bluegrass & Folk Festival</title>
         <meta
-          name="Bridgetown Bluegrass & Folk Festival"
+          name='Bridgetown Bluegrass & Folk Festival'
           content="Portland, Oregon's very own bluegrass and folk music festival"
-          // content='bluegrass Portland music festival Bridgetown Folk PDX non-profit nonprofit live ethos program'
+
+          // content='bluegrass Portland music festival Bridgetown Folk PDX non-profit nonprofit live ethos program Never Come Down Cascade Crescendo Alder Street Five Letter Word Hot Damn Scandal Fog Holler The Muddy Souls Chicken $#!T Gamblers Amanda Richards & The Good Long Whiles Alison Self Bitches in the Beehive Siena Christie Blue Flags & Black Grass The Lois and Clark Expedition Jasper Diem One Grass Two Grass Laney Lou & the Bird Dogs Left Coast Country Scratchdog Stringband Warren G. Hardings Quattlebaum Hill Williams Julie and the Wayves Mountain Honey Lillian Sawyer Band High Seagrass Wooden Sleepers The Ramblin Kind Out West Whiskey Deaf Whistlin Rufus The Licklog Disputes TIm Connell Eric Skye Mighty Dreadful Stringband Caitlin Jemma & the Goodness The Hollerbodies Slabtown Rounders'
         />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
